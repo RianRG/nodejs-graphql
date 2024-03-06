d9c30ea2400739f972b3e94b428cecef596b0bdb
rian <rianrgomes@gmail.com>
2024-03-06 16:22:45 -0300

    initialize schema.gql

--- src/resolvers/student.resolver.ts
+++ src/resolvers/student.resolver.ts
@@ -1,11 +1,15 @@
 import { Query, Resolver } from "type-graphql";
 import { Student } from "../models/student-model";
+import { PrismaService } from "../services/prisma.service";
+
+//not using dependecy injection..
+const prisma = new PrismaService()
 
 @Resolver()
 export class StudentResolver{
   
   @Query(() => [Student])
   async listStudents(){
-    return 
+    return prisma.listStudents();
   }
 }
--- src/services/prisma.service.ts
+++ src/services/prisma.service.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 export class PrismaService extends PrismaClient{
-
+  async listStudents(){
+    // return await this.student.findMany();
+  }
 }
